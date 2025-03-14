142a267929b83b9ac8cc096fe0a90b53dd5e8d77
ShaunGruuw <111718169+ShaunGruuw@users.noreply.github.com>
2025-03-14 10:26:35 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true, // Ignores ESLint errors during build
+    },
+    typescript: {
+        ignoreBuildErrors: true, // Ignores TypeScript errors during build
+    },
 };
 
 export default nextConfig;
